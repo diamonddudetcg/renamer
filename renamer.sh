9b1ac907d25ed6b6a14da2d27bb8b31f474c00bb
Diamond Dude <santirivera92@gmail.com>
2023-12-12 04:43:37 +0100

    Renamer

---
+++ renamer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the current directory of the script
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change directory to the script directory
+cd "$script_dir" || exit
+
+# Loop through each file in the current directory
+for file in *; do
+    # Check if the file is a regular file (not a directory)
+    if [ -f "$file" ]; then
+        # Check if the file contains the specified string
+        if grep -q "1P Episode" "$file"; then
+            # Rename the file, replacing "1P Episode" with "One Piece -"
+            new_name=$(echo "$file" | sed 's/1P Episode/One Piece -/')
+            mv "$file" "$new_name"
+            echo "Renamed: $file -> $new_name"
+        fi
+    fi
+done
